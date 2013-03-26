ed294c5acec09cd04883408dfec1603a090b1ae4
Austin Hendrix <ahendrix@willowgarage.com>
2013-03-25 18:19:30 -0700

    Fix catkinization of calibration_estimation.

--- calibration_estimation/setup.py
+++ calibration_estimation/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['calibration_estimation', 'urdf_python'],
+    packages=['calibration_estimation', 'calibration_estimation.sensors', 'urdf_python'],
     package_dir={'': 'src'},
     install_requires=['genpy', 'numpy', 'rosgraph', 'roslib', 'rospkg'],
     )
